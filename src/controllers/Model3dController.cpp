ee09db246a2fdaf75209364f483ddece71166676
Gregoire Guémas <gregoire.guemas@navispeed.eu>
2018-02-27 18:57:08 -0500

    Reformat

--- src/controllers/Model3dController.cpp
+++ src/controllers/Model3dController.cpp
@@ -1,231 +1,225 @@
 #include "Model3dController.h"
 #include <services/history/HistoryManager.h>
 
-Model3dController::~Model3dController()
-{
-	delete model1;
-	delete model2;
-	delete initialPoint;
+Model3dController::~Model3dController() {
+    delete model1;
+    delete model2;
+    delete initialPoint;
 }
 
-void Model3dController::setup()
-{
-	ofSetWindowTitle("3d mode");
-	ofDisableAlphaBlending();
-	this->history = HistoryManager::getInstance()->getFromController(this);
-	model1 = new ofxAssimpModelLoader();
-	model1->loadModel("model1.obj");
-	model2 = new ofxAssimpModelLoader();
-	model2->loadModel("model2.obj");
-	enableEvents();
-	light.enable();
-	light.setPosition(ofVec3f(100, 100, 200));
-	light.lookAt(ofVec3f(0, 0, 0));
+void Model3dController::setup() {
+    ofSetWindowTitle("3d mode");
+    ofDisableAlphaBlending();
+    this->history = HistoryManager::getInstance()->getFromController(this);
+    model1 = new ofxAssimpModelLoader();
+    model1->loadModel("model1.obj");
+    model2 = new ofxAssimpModelLoader();
+    model2->loadModel("model2.obj");
+    enableEvents();
+    light.enable();
+    light.setPosition(ofVec3f(100, 100, 200));
+    light.lookAt(ofVec3f(0, 0, 0));
 }
 
-void Model3dController::draw()
-{
-	ofEnableDepthTest();
-	for (auto &it : container) {
-		it->drawObject();
-	}
+void Model3dController::draw() {
+    ofEnableDepthTest();
+    for (auto &it : container) {
+        it->drawObject();
+    }
 }
 
 void Model3dController::enableEvents() {
-	ofAddListener(ofEvents().mousePressed, this, &Model3dController::onMousePressed);
-	ofAddListener(ofEvents().mouseReleased, this, &Model3dController::onMouseReleased);
-	ofAddListener(ofEvents().keyPressed, this, &Model3dController::onKeyRelease);
+    ofAddListener(ofEvents().mousePressed, this, &Model3dController::onMousePressed);
+    ofAddListener(ofEvents().mouseReleased, this, &Model3dController::onMouseReleased);
+    ofAddListener(ofEvents().keyPressed, this, &Model3dController::onKeyRelease);
 }
 
 void Model3dController::disableEvents() {
-	ofRemoveListener(ofEvents().mousePressed, this, &Model3dController::onMousePressed);
-	ofRemoveListener(ofEvents().mouseReleased, this, &Model3dController::onMouseReleased);
-	ofRemoveListener(ofEvents().keyPressed, this, &Model3dController::onKeyRelease);
+    ofRemoveListener(ofEvents().mousePressed, this, &Model3dController::onMousePressed);
+    ofRemoveListener(ofEvents().mouseReleased, this, &Model3dController::onMouseReleased);
+    ofRemoveListener(ofEvents().keyPressed, this, &Model3dController::onKeyRelease);
 }
 
 void Model3dController::onMousePressed(ofMouseEventArgs &evt) {
-	ofVec3f position(evt.x, evt.y, 0);
-	switch (formMode) {
-	case FormMode::MODEL1: {
-		std::cout << "newModel1" << std::endl;
-		createModel(position, this->model1);
-		break;
-	}
-	case FormMode::MODEL2: {
-		ofxAssimpModelLoader * temp2 = model2;
-		std::cout << "newModel2" << std::endl;
-		createModel(position, this->model2);
-		break;
-	}
-	default:
-		ofVec2f * ptr = new ofVec2f(evt.x, evt.y);
-		if (this->initialPoint != nullptr) {
-			delete this->initialPoint;
-		}
-		initialPoint = ptr;
-		break;
-	}
+    ofVec3f position(evt.x, evt.y, 0);
+    switch (formMode) {
+        case FormMode::MODEL1: {
+            std::cout << "newModel1" << std::endl;
+            createModel(position, this->model1);
+            break;
+        }
+        case FormMode::MODEL2: {
+            ofxAssimpModelLoader *temp2 = model2;
+            std::cout << "newModel2" << std::endl;
+            createModel(position, this->model2);
+            break;
+        }
+        default:
+            ofVec2f *ptr = new ofVec2f(evt.x, evt.y);
+            if (this->initialPoint != nullptr) {
+                delete this->initialPoint;
+            }
+            initialPoint = ptr;
+            break;
+    }
 }
 
 void Model3dController::onKeyRelease(ofKeyEventArgs &evt) {
-	
-	ofPoint modelPosition(ofGetWidth() * 0.5, (float)ofGetHeight() * 0.75);
-	CursorManager::getInstance()->setCursor(CursorManager::CURSOR_TYPE::DEFAULT);
-	switch (evt.key) {
-	case 97://a
-		formMode = FormMode::MODEL1;
-		std::cout << "modeModel1" << std::endl;
-		break;
-	case 98://b
-		formMode = FormMode::MODEL2;
-		std::cout << "modeModel2" << std::endl;
-		break;
-	case 99://c
-		formMode = FormMode::BOX;
-		std::cout << "modeBoite" << std::endl;
-		CursorManager::getInstance()->setCursor(CursorManager::CURSOR_TYPE::CUBE);
-		break;
-	case 115://s
-		formMode = FormMode::SPHERE;
-		std::cout << "modeSphere" << std::endl;
-		CursorManager::getInstance()->setCursor(CursorManager::CURSOR_TYPE::SPHERE);
-		break;
-	case 356://fleche
-		for (int i = 0; i < selection.size(); i++) {
-			this->transform(selection[i].get(),1);
-		}
-		break;
-	case 358://fleche
-		for (int i = 0; i < selection.size(); i++) {
-			this->transform(selection[i].get(),-1);
-		}
-	case 90: {
-		auto it = container.end();
-		if (it != container.begin()) {
-			it--;
-			selection.clear();
-			selection.push_back(*it);
-		}
-		break;
-	}
-	case 114://r
-		this->transformType = TransformType::ROTATE;
-		break;
-	case 116://t
-		this->transformType = TransformType::TRANSLATE;
-		break;
-	case 112://p
-		this->transformType = TransformType::SCALE;
-		xScale = SCALEDISTANCE;
-		yScale = SCALEDISTANCE;
-		zScale = SCALEDISTANCE;
-		break;
-	case 120://x
-		xTranslate = TRANSLATEDISTANCE;
-		yTranslate = 0;
-		zTranslate = 0;
-		xRotate = ROTATEDISTANCE;
-		yRotate = 0;
-		zRotate = 0;
-		xScale = SCALEDISTANCE;
-		yScale = 0;
-		zScale = 0;
-		break;
-	case 121://y
-		xTranslate = 0;
-		yTranslate = TRANSLATEDISTANCE;
-		zTranslate = 0;
-		xRotate = 0;
-		yRotate = ROTATEDISTANCE;
-		zRotate = 0;
-		xScale = 0;
-		yScale = SCALEDISTANCE;
-		zScale = 0;
-		break;
-	case 122://z
-		xTranslate = 0;
-		yTranslate = 0;
-		zTranslate = TRANSLATEDISTANCE;
-		xRotate = 0;
-		yRotate = 0;
-		zRotate = ROTATEDISTANCE;
-		xScale = 0;
-		yScale = 0;
-		zScale = SCALEDISTANCE;
-		break;
-	case 127://supprimer
-		this->reset();
-		break;
-	default:
-		formMode = FormMode::NONE;
-		break;
-	}
+
+    ofPoint modelPosition(ofGetWidth() * 0.5, (float) ofGetHeight() * 0.75);
+    CursorManager::getInstance()->setCursor(CursorManager::CURSOR_TYPE::DEFAULT);
+    switch (evt.key) {
+        case 97://a
+            formMode = FormMode::MODEL1;
+            std::cout << "modeModel1" << std::endl;
+            break;
+        case 98://b
+            formMode = FormMode::MODEL2;
+            std::cout << "modeModel2" << std::endl;
+            break;
+        case 99://c
+            formMode = FormMode::BOX;
+            std::cout << "modeBoite" << std::endl;
+            CursorManager::getInstance()->setCursor(CursorManager::CURSOR_TYPE::CUBE);
+            break;
+        case 115://s
+            formMode = FormMode::SPHERE;
+            std::cout << "modeSphere" << std::endl;
+            CursorManager::getInstance()->setCursor(CursorManager::CURSOR_TYPE::SPHERE);
+            break;
+        case 356://fleche
+            for (int i = 0; i < selection.size(); i++) {
+                this->transform(selection[i].get(), 1);
+            }
+            break;
+        case 358://fleche
+            for (int i = 0; i < selection.size(); i++) {
+                this->transform(selection[i].get(), -1);
+            }
+        case 90: {
+            auto it = container.end();
+            if (it != container.begin()) {
+                it--;
+                selection.clear();
+                selection.push_back(*it);
+            }
+            break;
+        }
+        case 114://r
+            this->transformType = TransformType::ROTATE;
+            break;
+        case 116://t
+            this->transformType = TransformType::TRANSLATE;
+            break;
+        case 112://p
+            this->transformType = TransformType::SCALE;
+            xScale = SCALEDISTANCE;
+            yScale = SCALEDISTANCE;
+            zScale = SCALEDISTANCE;
+            break;
+        case 120://x
+            xTranslate = TRANSLATEDISTANCE;
+            yTranslate = 0;
+            zTranslate = 0;
+            xRotate = ROTATEDISTANCE;
+            yRotate = 0;
+            zRotate = 0;
+            xScale = SCALEDISTANCE;
+            yScale = 0;
+            zScale = 0;
+            break;
+        case 121://y
+            xTranslate = 0;
+            yTranslate = TRANSLATEDISTANCE;
+            zTranslate = 0;
+            xRotate = 0;
+            yRotate = ROTATEDISTANCE;
+            zRotate = 0;
+            xScale = 0;
+            yScale = SCALEDISTANCE;
+            zScale = 0;
+            break;
+        case 122://z
+            xTranslate = 0;
+            yTranslate = 0;
+            zTranslate = TRANSLATEDISTANCE;
+            xRotate = 0;
+            yRotate = 0;
+            zRotate = ROTATEDISTANCE;
+            xScale = 0;
+            yScale = 0;
+            zScale = SCALEDISTANCE;
+            break;
+        case 127://supprimer
+            this->reset();
+            break;
+        default:
+            formMode = FormMode::NONE;
+            break;
+    }
 }
 
-void Model3dController::onMouseReleased(ofMouseEventArgs & evt)
-{	
-	switch (formMode) {
-	case FormMode::BOX:
-		this->createBox(ofVec3f(evt.x, evt.y, 0),*(this->initialPoint));
-		break;
-	case FormMode::SPHERE:
-		this->createSphere(ofVec3f(evt.x, evt.y, 0), *(this->initialPoint));
-		break;
-	default:
-		break;
-	}
+void Model3dController::onMouseReleased(ofMouseEventArgs &evt) {
+    switch (formMode) {
+        case FormMode::BOX:
+            this->createBox(ofVec3f(evt.x, evt.y, 0), *(this->initialPoint));
+            break;
+        case FormMode::SPHERE:
+            this->createSphere(ofVec3f(evt.x, evt.y, 0), *(this->initialPoint));
+            break;
+        default:
+            break;
+    }
 }
 
-void Model3dController::createModel(const ofVec3f &position,ofxAssimpModelLoader * model) {
-	auto redoFunction = [model, position, this]() { this->createModel(position, model); };
-	this->addItem(std::make_shared<Model3d>(model, position));
-	DEFINE_UNDO_REDO_CONTAINER(this->history, this->container, redoFunction);
+void Model3dController::createModel(const ofVec3f &position, ofxAssimpModelLoader *model) {
+    auto redoFunction = [model, position, this]() { this->createModel(position, model); };
+    this->addItem(std::make_shared<Model3d>(model, position));
+    DEFINE_UNDO_REDO_CONTAINER(this->history, this->container, redoFunction);
 }
 
-void Model3dController::createBox(const ofVec3f & position, const ofVec2f& startPoint)
-{
-	auto temp = new ofBoxPrimitive();
-	temp->set(startPoint.distance(ofVec2f(position.x, position.y)));
-	temp->setPosition(position);
-	auto ptr = std::make_shared<Primitive3d>(temp);
-	this->addItem(ptr);
+void Model3dController::createBox(const ofVec3f &position, const ofVec2f &startPoint) {
+    auto temp = new ofBoxPrimitive();
+    temp->set(startPoint.distance(ofVec2f(position.x, position.y)));
+    temp->setPosition(position);
+    auto ptr = std::make_shared<Primitive3d>(temp);
+    this->addItem(ptr);
 
-	auto redoFunction = [position, this,startPoint]() { this->createBox(position,startPoint); };
-	DEFINE_UNDO_REDO_CONTAINER(this->history, this->container, redoFunction);
+    auto redoFunction = [position, this, startPoint]() { this->createBox(position, startPoint); };
+    DEFINE_UNDO_REDO_CONTAINER(this->history, this->container, redoFunction);
 }
 
-void Model3dController::createSphere(const ofVec3f & position, const ofVec2f& startPoint)
-{
-	auto temp = new ofSpherePrimitive();
-	temp->setRadius((startPoint.distance(ofVec2f(position.x, position.y))) / 2);
-	temp->setPosition(position);
-	auto ptr = std::make_shared<Primitive3d>(temp);
-	this->addItem(ptr);
+void Model3dController::createSphere(const ofVec3f &position, const ofVec2f &startPoint) {
+    auto temp = new ofSpherePrimitive();
+    temp->setRadius((startPoint.distance(ofVec2f(position.x, position.y))) / 2);
+    temp->setPosition(position);
+    auto ptr = std::make_shared<Primitive3d>(temp);
+    this->addItem(ptr);
 
-	auto redoFunction = [position, this, startPoint]() { this->createSphere(position, startPoint); };
-	DEFINE_UNDO_REDO_CONTAINER(this->history, this->container, redoFunction);
+    auto redoFunction = [position, this, startPoint]() { this->createSphere(position, startPoint); };
+    DEFINE_UNDO_REDO_CONTAINER(this->history, this->container, redoFunction);
 }
 
-void Model3dController::transform(Object3d* obj,int direction) {
-	switch (transformType) {
-	case TransformType::ROTATE:
-		obj->rotate(direction*5, ofVec3f(xRotate, yRotate, zRotate));
-		break;
-	case TransformType::SCALE:
-		obj->modifyScale(ofVec3f(direction*xScale, direction*yScale, direction*zScale));
-		break;
-	case TransformType::TRANSLATE:
-		obj->translate(ofVec3f(direction * xTranslate, direction *yTranslate, direction *zTranslate));
-		break;
-	}
+void Model3dController::transform(Object3d *obj, int direction) {
+    switch (transformType) {
+        case TransformType::ROTATE:
+            obj->rotate(direction * 5, ofVec3f(xRotate, yRotate, zRotate));
+            break;
+        case TransformType::SCALE:
+            obj->modifyScale(ofVec3f(direction * xScale, direction * yScale, direction * zScale));
+            break;
+        case TransformType::TRANSLATE:
+            obj->translate(ofVec3f(direction * xTranslate, direction * yTranslate, direction * zTranslate));
+            break;
+    }
 }
 
 void Model3dController::addItem(Object3d_Ptr ptr) {
-	container.push_back(ptr);
-	selection.clear();
-	selection.push_back(ptr);
+    container.push_back(ptr);
+    selection.clear();
+    selection.push_back(ptr);
 }
 
 void Model3dController::reset() {
-	container.clear();
+    container.clear();
 }
